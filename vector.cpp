f80773a5ecbe968ed0bac2eba89ca233021f5424
yousefsrhan <143297067+yousefsrhan@users.noreply.github.com>
2025-03-03 23:59:57 +0200

    Create vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+//#include <vector>
+using namespace std;
+
+class  Vector
+{
+    private:
+        int *arr= nullptr;
+        int size = 0;
+    public:
+        Vector(int Size) : size(Size<= 0 ? 1: Size), arr( new int [size])  {}
+        ~Vector(){ delete [] arr ; arr = nullptr;}
+        int getByIndex(int index){
+            if (index >=0)
+            return arr[index];
+            return -1;
+            }
+        int getSize(){
+            return this->size;
+            }
+        void setVal(int index,int value){
+             this->arr[index]= value;
+            }
+        int find(int value)
+            {
+                for( int i=0 ; i < getSize(); ++i)
+                        if (  arr[i] == value)
+                            return i;
+                return -1;
+            }
+        void print()
+            {
+                for( int i=0 ; i<getSize(); ++i)
+                    cout << arr[i]<<" ";
+                    cout<< endl;
+            }
+        int getFront(){
+            return arr[0];
+            }
+            int getBack(){
+            return arr[size-1];   // don't forget to do minus 1
+            }
+            // let's push_back[[ version 1.0 ]]
+            // 1-Always create other bigger *arr2
+            // 2- copy
+            // 3- append item at [size]
+            // 4-swap (to swap pointers) to keep arr1 in charge
+            // 5-delete[]arr2
+        void push_back_noCapacityTrick(int value){
+            int *arr2 = new int [size+1];// steps = size +1
+            //copy
+            for(int i=0 ; i<size;++i) // size steps
+                arr2[i] =arr[i];
+            arr2[size++] = value; //2 steps
+          //size++; // in previous step we increased size and added the newValue
+            swap(arr2, arr);// 3 steps
+            delete [] arr2;
+            } // this push_back has o(n) operation [[ LINEAR ]]
+        // create new *ptr
+        // copy elememnts from 0 to newsize index -1 to arr
+        // swap pointers
+        // delete nd ptr
+        void resize(int newSize){
+            int *arr2 = new int [newSize];
+            for(int i=0 ; i<newSize;++i){
+                arr2[i] = arr[i];
+            }
+            swap(arr2,arr);
+            delete [] arr2;
+            }
+            // insertion is based on shifting elements
+            // we shift from end to beginning >> but why?
+            void insert(int index,int val){
+            int *arr2 = new int [size*2];
+            for(int i =0 ; i< size+1;++i){
+                
+                    }// end for loop
+            }// end insert func
+
+};
+
+int main()
+{
+   return 0;
+}
