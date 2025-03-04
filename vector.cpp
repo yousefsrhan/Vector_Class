aff27f6bc1e598dc853315ceff8b4d0f9fa7dddc
yousefsrhan <143297067+yousefsrhan@users.noreply.github.com>
2025-03-05 01:23:13 +0200

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -9,7 +9,7 @@ private:
 	int* arr = nullptr;
 public:
 	Vector(int size) :
-	size(size >= 0 ? size : 0)
+		size(size > 0 ? size : 0)
 	, capacity(size > 0 ? 2 * size : 1)
 	, arr(new int[capacity]) {}
 	~Vector() {
@@ -40,7 +40,7 @@ public:
 				  return i ;
 		  return -1;
 	  }
-	  // change size/capacity -add/delete/insert  functions, count [2]
+	  // change size/capacity -add/popLast/insert  functions, count [5]
 	  void expandCapacity() {
 		  capacity= 2 * capacity;
 		  int* arr2 = new int[capacity];
@@ -54,7 +54,7 @@ public:
 		  if (size == capacity) {
 			  expandCapacity();
 		  }
-		  arr[size++] = val;
+		  arr[size++] = val;// no garbage here because size starts from zero so the garbage is cleaned!
 	  }
 	  int popLast() {
 		  int temp = arr[size - 1];
@@ -96,28 +96,39 @@ public:
 				  cout << arr[i] << " ";
 			  cout << endl;
 		  }
+		  void rightRotation()
+		  {
+			  int temp = arr[size - 1];
+			  for (int i = size - 1; i >= 1;--i)
+				  arr[i] = arr[i - 1];
+			  arr[0] = temp;
+			  
+			  //swap(arr[0], arr[size - 1]);
+		  }
 };
 int main()
 {
 	Vector v(0);
 	v.push_back(1);
 	v.push_back(2);
-	v.push_back(3);
 	v.push_back(4);
 	v.push_back(5);
+	v.insert(2, 3);
+	v.popLast();
 	v.display();
+
 	// output 1 2 3 4 5
-	vector<int> myVector(5);
-	myVector.push_back(1);
-	myVector.push_back(2);
-	myVector.push_back(3);
-	myVector.push_back(4);
-	myVector.push_back(5);
-	for (int i = 0; i < 5; ++i)
-		cout << myVector[i]<<" ";
-	cout<< endl;
-	// output 0 0 0 0 0
-	for (int i = 0; i < 10; ++i)
-		cout << myVector[i]<<" ";
-		// output 0 0 0 0 0 1 2 3 4 5
+	//vector<int> myVector(5);
+	//myVector.push_back(1);
+	//myVector.push_back(2);
+	//myVector.push_back(3);
+	//myVector.push_back(4);
+	//myVector.push_back(5);
+	//for (int i = 0; i < 5; ++i)
+	//	cout << myVector[i]<<" ";
+	//cout<< endl;
+	//// output 0 0 0 0 0
+	//for (int i = 0; i < 10; ++i)
+	//	cout << myVector[i]<<" ";
+	//	// output 0 0 0 0 0 1 2 3 4 5
 }
